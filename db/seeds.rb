3664c319a31be0d89539adf5225d90e8bc8b2d14
apalaciosc <apalacioscarrillo@gmail.com>
2018-10-03 18:15:47 -0500

    FIX: Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,7 @@ cardiologia = Area.find_by(name:'cardiología')
 if TypeExam.all.count == 0
   #exámenes
   TypeExam.create(area_id:examen_medico.id ,name: 'C.A.M.O', view: 'camo')
+  TypeExam.create(area_id:examen_medico.id ,name: 'Resumen exámen ocupacional', view: 'occupational_health_summary')
   TypeExam.create(area_id:examen_medico.id ,name: 'exámen médico', view: 'medical_exam')
   TypeExam.create(area_id:examen_medico.id ,name: 'exámen físico', view: 'physical_exam')
   TypeExam.create(area_id:examen_medico.id ,name: 'evaluación ergonómica', view: 'ergonomic_evaluation')
