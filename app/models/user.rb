16994aade10436cfed9d1c01940b9e9f0ce4cc9e
apalaciosc <apalacioscarrillo@gmail.com>
2018-10-15 08:19:14 -0500

    FIX: Medical

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  belongs_to :medical
+  belongs_to :medical, optional:true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
