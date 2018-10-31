5591629d56fcdf3e1a95db026687ce78f57d457f
cxasper <cxasper23@gmail.com>
2018-10-31 01:23:18 -0500

    Set environment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,26 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     user ||= User.new # guest user (not logged in)
+
+    alias_action :tree, to: :read
+    alias_action :create, :read, :update, to: :cru
+    alias_action :create, :update, :delete, to: :cud
+    alias_action :create, :read, :update, :delete, to: :crud
+
     if user.type_user == 'admin'
       can :manage, :all
-    elsif user.type_user == 'recep'
+    elsif user.type_user == 'recepcionist'
       can :read, :all
+      can :crud, Attention
+      can :read, Employee
+      can :read, Enterprise
+      can :cru, Exam
+      can :cru, TypeExam
     elsif user.type_user == 'medical'
-      can :read, :all
+      can :read, Employee
+      can :read, Attention
+      can :cru, Exam
+      can :cru, TypeExam
     end
 
     # The first argument to `can` is the action you are giving the user
