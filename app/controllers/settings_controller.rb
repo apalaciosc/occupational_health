fab167b8fa20d64927dcf879174b4928d6129ece
apalaciosc <apalacioscarrillo@gmail.com>
2018-11-07 20:15:36 -0500

    FIX: Permission settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,11 +2,11 @@ class SettingsController < ApplicationController
   before_action :setting, only: [:config_settings, :save_settings, :data_license]
 
   def config_settings
-    redirect_to root_path unless verify_is_super_admin
+    redirect_to root_path unless verify_is_admin
   end
 
   def save_settings
-    redirect_to root_path unless verify_is_super_admin
+    redirect_to root_path unless verify_is_admin
     if @setting.update(params[:setting].permit!)
       flash[:notice] = "Configuración actualizada correctamente."
       flash.keep(:notice)
@@ -21,8 +21,8 @@ class SettingsController < ApplicationController
   end
 
   protected
-  def verify_is_super_admin
-    return true if user_signed_in? && current_user.superadmin?
+  def verify_is_admin
+    return true if user_signed_in? && (current_user.superadmin? || current_user.admin?)
     return false
   end
   def setting
